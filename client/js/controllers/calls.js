1c69521dfa941347256f2cb7255d8729dc456322
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-13 11:43:29 -0800

    Sample data

--- client/js/controllers/calls.js
+++ client/js/controllers/calls.js
@@ -1,3 +1,6 @@
 angular.module('fifteenAppControllers').controller('CallsController', ['$scope', 'DataManager', function($scope, DataManager) {
-
+	DataManager.fetchAll('Call')
+		.then(function(providers) {
+			$scope.providers = providers;
+		});
 }]);
--- client/js/controllers/providers.js
+++ client/js/controllers/providers.js
@@ -1,3 +1,6 @@
 angular.module('fifteenAppControllers').controller('ProvidersController', ['$scope', 'DataManager', function($scope, DataManager) {
-
+	DataManager.fetchAll('Provider')
+		.then(function(providers) {
+			$scope.providers = providers;
+		});
 }]);
