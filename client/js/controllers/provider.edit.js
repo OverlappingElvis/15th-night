6a52bf59ebd2afed0045b7ffb5e09688ff8abc09
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-13 12:19:26 -0800

    Fetch and edit provider

--- client/js/controllers/provider.edit.js
+++ client/js/controllers/provider.edit.js
@@ -1,3 +1,11 @@
-angular.module('fifteenAppControllers').controller('ProviderEditController', ['$scope', 'DataManager', function($scope, DataManager) {
-
+angular.module('fifteenAppControllers').controller('ProviderEditController', ['$scope', 'DataManager', '$state', '$stateParams', function($scope, DataManager, $state, $stateParams) {
+	var providerId = parseInt($stateParams.id, 10);
+	var fetchProvider = function() {
+		return DataManager.fetchOne('Provider', providerId);
+	};
+	$scope.save = function() {
+		DataManager.updateOne('Provider', providerId)
+			.then(fetchProvider)
+	};
+	fetchProvider();
 }]);
