94d34ab47601007a7522eed35086c870c6b412a3
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-12 22:27:00 -0800

    Front and back

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,41 @@
 angular.module('fifteenApp', [
 		'ui.router',
 		'lbServices'
-	]);
+	])
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+		$urlRouterProvider.otherwise('/');
+		$stateProvider.state('login', {
+				url: '/login',
+				controller: 'LoginController',
+				templateUrl: 'views/login.html'
+			})
+			.state('logout', {
+				url: '/logout',
+				controller: 'LogoutController'
+			})
+			.state('dashboard', {
+				url: '/dashboard',
+				controller: 'DashboardController',
+				templateUrl: 'views/dashboard.html',
+				authenticate: true
+			});
+		$locationProvider.html5Mode(false);
+		$locationProvider.hashPrefix('!');
+	}])
+	.run(['$rootScope', 'DataManager', '$state', function($rootScope, DataManager, $state) {
+		$rootScope.$on('$stateChangeStart', function(e, next) {
+			if (!next.authenticate) {
+				return;
+			};
+			DataManager.modelMethod('Profile', 'getCurrent')
+				.then(_.noop, function() {
+					e.preventDefault();
+					delete $rootScope.currentUser;
+					$state.go('login');
+				});
+		});
+		return DataManager.modelMethod('Profile', 'getCurrent')
+			.then(function(profile) {
+				$rootScope.currentUser = profile;
+			});
+	}]);
--- client/js/controllers/page.js
+++ client/js/controllers/page.js
@@ -1,5 +1,3 @@
 angular.module('fifteenApp').controller('PageController', ['$scope', '$state', function($scope, $state) {
-	$scope.className = function() {
-		return $state.is('login') ? 'login-page' : 'skin-blue sidebar-mini';
-	};
+	$scope.isState = $state.is;
 }]);
