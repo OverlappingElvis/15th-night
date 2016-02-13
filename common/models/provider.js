02114d90daf4ef3d095750c475bc379f98ed04da
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-12 20:42:40 -0800

    Bower

--- common/models/provider.js
+++ common/models/provider.js
@@ -1,3 +1,16 @@
 module.exports = function(Provider) {
+	var request = require('request');
+	Provider.prototype.sendMessage = function(message, cb) {
 
+	};
+	Provider.remoteMethod('sendMessage', {
+		isStatic: false,
+		http: {
+			verb: 'post'
+		},
+		accepts: {
+			arg: 'message',
+			type: 'string'
+		}
+	})
 };
