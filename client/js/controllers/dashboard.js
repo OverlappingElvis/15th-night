1ecb3ba70eaf2c71f65966e145637fc7e6056880
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-13 13:18:28 -0800

    Add Properties

--- client/js/controllers/dashboard.js
+++ client/js/controllers/dashboard.js
@@ -1,3 +1,28 @@
 angular.module('fifteenAppControllers').controller('DashboardController', ['$scope', 'DataManager', function($scope, DataManager) {
 
+	$scope.messagePreview = function() {
+		var message = $scope.call.message;
+		if (!message) {
+			return '...';
+		}
+		return '15thNight: ' + $scope.call.message + ' REPLY YES TO ACCEPT';
+	};
+
+	$scope.resetCall = function() {
+		$scope.call = {};
+	};
+
+	DataManager.fetchAll('Provider')
+		.then(function(providers) {
+			$scope.providers = providers;
+			$scope.providerCategories = _(providers).chain()
+				.pluck('category')
+				.map(function(categories) {
+					return categories.split(',');
+				})
+				.flatten()
+				.uniq()
+				.value()
+				.sort();
+		});
 }]);
