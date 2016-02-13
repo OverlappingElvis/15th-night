e65be68fe6abd58ec288f30f0c422a1e0250eea8
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-13 15:37:35 -0800

    Tropo changes

--- common/models/call.js
+++ common/models/call.js
@@ -1,8 +1,7 @@
 module.exports = function(Call) {
 	var app = require('../../server/server');
 	var http = require('http');
-	// Untracked tokens file
-	var tokens = require('../../server/tokens');
+	var _ = require('underscore');
 
 	Call.observe('before save', function(context, next) {
 		if (!context.isNewInstance) {
@@ -13,24 +12,7 @@ module.exports = function(Call) {
 		var recipients = instance.recipients;
 		recipients.each(function(recipientId) {
 			app.models.Provider.findById(recipientId, function(err, provider) {
-				if (!provider.phone) {
-					return;
-				}
-				var number = provider.phone;
-				var message = encodeURI(instance.message);
-				var path = '/1.0/sessions?action=create&token=' + tokens.tropo.messaging + '&msg=' + message + '&number=' + number;
-				var tropoSessionAPI = 'api.tropo.com';
-				var tropo = http.createClient(80, tropoSessionAPI);
-				var request = tropo.request('GET', path, {'host': tropoSessionAPI});
-				var request = tropo.request('GET', path, {'host': tropoSessionAPI});
-				request.end();
-				request.on('response', function (response) {
-					response.setEncoding('utf8');
-					response.addListener('data', function (chunk) {
-						console.log('Sent message. Tropo response code:' + response.statusCode + '. Body: ' + chunk);
-					});
-				});
-
+				provider.sendMessage(instance.message, _.noop);
 			});
 		});
 		next();
--- common/models/provider.js
+++ common/models/provider.js
@@ -1,7 +1,26 @@
 module.exports = function(Provider) {
-	var request = require('request');
-	Provider.prototype.sendMessage = function(message, cb) {
-
+	var http = require('http');]
+	// Untracked tokens file
+	var tokens = require('../../server/tokens');
+	Provider.prototype.sendMessage = function(message, next) {
+		var provider = this;
+		if (!provider.phone) {
+			return;
+		}
+		var number = provider.phone;
+		var message = encodeURI(instance.message);
+		var path = '/1.0/sessions?action=create&token=' + tokens.tropo.messaging + '&msg=' + message + '&number=' + number;
+		var tropoSessionAPI = 'api.tropo.com';
+		var tropo = http.createClient(80, tropoSessionAPI);
+		var request = tropo.request('GET', path, {'host': tropoSessionAPI});
+		request.end();
+		request.on('response', function (response) {
+			response.setEncoding('utf8');
+			response.addListener('data', function (chunk) {
+				console.log('Sent message. Tropo response code:' + response.statusCode + '. Body: ' + chunk);
+			});
+		});
+		next();
 	};
 	Provider.remoteMethod('sendMessage', {
 		isStatic: false,
