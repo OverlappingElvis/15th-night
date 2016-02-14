415c10534fe0c5f415353a679c4076c1461be63a
Jeremy Sher <OverlappingElvis@gmail.com>
2016-02-13 22:46:41 -0800

    call script

--- tropo/voicemessage.js
+++ tropo/voicemessage.js
@@ -1,17 +1,15 @@
 call(number);
 say('This is an automated call from Fifteenth Night');
 say('on behalf of');
-say(reply.split().join(' '))
+say(reply.split('').join(' '))
 say('Message follows.');
 wait(750);
 say(msg);
-var result = ask('Please press one to be connected to the sender', {
-	choices: '[1 DIGIT]'
+var result = ask('If you would like to be connected to the sender, say connect. Otherwise, you may hang up.', {
+	choices: 'connect',
+	onChoice(function(event) {
+		say('Connecting you now.')
+		transfer('reply');
+	})
 });
-if (result.value === 1) {
-	say('Transferring you now.');
-	transfer(reply);
-} else {
-	say(result.value);
-	say('Goodbye.');
-}
+
